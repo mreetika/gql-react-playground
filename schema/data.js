a7041b4cdd603962c7d4bb1bc88129f03a00692f
mreetika <puja_ecse@yahoo.com>
2019-07-04 21:10:01 -0600

    initial commit

---
+++ schema/data.js
@@ -0,0 +1,71 @@
+const books = [{
+    name: "GingerBread",
+    author: "Helen Oyeyemi",
+    genre: "Fantasy",
+    id: '1',
+    authorId: '1'
+  },
+  {
+    name: "Normal People",
+    author: "Sally Rooney",
+    genre: "Fiction",
+    id: '2',
+    authorId: '2'
+  },
+  {
+    name: "The Water Cure",
+    author: "Sophie Mackintosh",
+    genre: "Fiction",
+    id: '3',
+    authorId: '3'
+  },
+  {
+    name: "Star",
+    author: "Yukio Mishima",
+    genre: "Sci-Fi",
+    id: '4',
+    authorId: '4'
+  },
+  {
+    name: "Star 23",
+    author: "Sally Rooney",
+    genre: "Fiction",
+    id: '4',
+    authorId: '2'
+  },
+  {
+    name: "Bellevuw",
+    author: "Sophie Mackintosh",
+    genre: "Adventure",
+    id: '4',
+    authorId: '3'
+  }
+];
+
+const authors = [{
+    name: "Helen Oyeyemi",
+    age: 25,
+    id: '1',
+    bookId: '1'
+  },
+  {
+    name: "Sally Rooney",
+    age: 46,
+    id: '2',
+    bookId: '2'
+  },
+  {
+    name: "Sophie Mackintosh",
+    age: 29,
+    id: '3',
+    bookId: '3'
+  },
+  {
+    name: "Yukio Mishima",
+    age: 37,
+    id: '4',
+    bookId: '4'
+  }
+];
+
+module.exports = [books, authors]
